ac16cd8f59b50dd1b6196191dbcbae8f5bfbcfc8
Arzenaakter <arzenaakter@gmail.com>
2024-05-01 10:55:50 +0600

    create first typescript module

---
+++ Typescript-Technocrat/module1/src/1.4.ts
@@ -0,0 +1,38 @@
+// primitive type
+
+let firstName: string = "arzena"
+
+let age: number = 25
+
+let isTrue: boolean = true
+
+let x: undefined = undefined
+let y: null = null
+
+// non primitive data types
+
+
+// 1. array
+
+let friends: string[] = ['hola', 'kola']
+
+
+let numbers: number[] = [1, 2, 3, 4]
+
+// touple
+
+let arr:[number, string] = [ 20, "arzena"]
+
+
+const user: {
+    name: string,
+    age: number,
+    isMarried?: boolean, // optional 
+    job: 'marthala'      // literal
+} = {
+    name: 'arzena',
+    age: 24,
+    isMarried: false,
+    job : 'marthala'
+
+}
